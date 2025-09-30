6034290a9b2495f66b7710f433b2f086adc5aa11
mohamedslman20131986-hash <mohamed.slman2013.1986@gmail.com>
2025-09-30 19:10:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,36 +8,12 @@ const ASSETS = [
   '/manifest.json'
 ];
 
-// install
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
-// activate (حذف الكاش القديم)
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// fetch
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(r => {
-      return r || fetch(e.request).catch(() => {
-        // fallback (مثلاً عرض صفحة offline إذا حاب تضيفها)
-        if (e.request.mode === 'navigate') {
-          return caches.match('/index.html');
-        }
-      });
-    })
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });

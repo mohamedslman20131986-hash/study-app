e65947659117606a80caa4c970b3dc19ad0f9151
mohamedslman20131986-hash <mohamed.slman2013.1986@gmail.com>
2025-10-03 21:22:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,13 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("v1").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/main.js",
-        "/timer.js",
-        "/db.js",
-        "/offline.html"
-      ]);
+    caches.open("library-cache").then(cache => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/main.js", "/add.js", "/db.js", "/offline.html"]);
     })
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request).then(res => res || caches.match("/offline.html")))
+    fetch(e.request).catch(() => caches.match(e.request).then(r => r || caches.match("/offline.html")))
   );
 });
